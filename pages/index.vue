ba38b5a5bd0887a5b86ae8a79df900e7049f6158
Andriy <devsokol@ukr.net>
2020-01-17 20:15:21 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <h2>Hello world</h2>
+  </section>
+</template>
+
+<script>
+// import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+}
+</script>
+
+<style>
+</style>
+
